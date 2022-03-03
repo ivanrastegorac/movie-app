61eb4f3fe89a384d37399bdd72507ca082e7fa44
Ivan Raštegorac <ivan.rastegorac@gmail.com>
2022-03-03 14:24:04 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavorite from './components/AddFavorites';
+import RemoveFavorites from './components/RemoveFavorites';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [favorites, setFavorites] = useState([]);
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=a1f9e237`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favorites', JSON.stringify(items));
+  };
+
+  const AddFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites, movie];
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  };
+
+  const removeFavoriteMovie = (movie) => {
+    const newFavoriteList = favorites.filter(
+      (favorite) => favorite.imdbID !== movie.imdbID
+    );
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  };
+
+  return (
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={movies}
+          handleFavoritesClick={AddFavoriteMovie}
+          favoriteComponent={AddFavorite}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favorites" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={favorites}
+          handleFavoritesClick={removeFavoriteMovie}
+          favoriteComponent={RemoveFavorites}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MovieList.js
@@ -0,0 +1,20 @@
+const MovieList = (props) => {
+  const FavoriteComponent = props.favoriteComponent;
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        <div className="image-container d-flex justify-content-start m3">
+          <img src={movie.Poster} alt="movie" />
+          <div
+            onClick={() => props.handleFavoritesClick(movie)}
+            className="overlay d-flex align-item-center justify-content-center"
+          >
+            <FavoriteComponent />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default MovieList;
